7325f0d87f7ff7cd35508d661a41fc1e45f1a5b7
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2024-10-22 20:08:13 +0530

    Fixed DB Issues

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectDatabase = () => {
-    mongoose.connect("mongodb+srv://suyashgupta:<NcyWcljRbw7pp5hy>@cluster0.9sxnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://suyashgupta:NcyWcljRbw7pp5hy@cluster0.9sxnw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log("Database connected successfully...");
     })
